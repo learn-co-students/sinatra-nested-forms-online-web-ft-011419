eaf5a8d54f0710d8050791538eaa74b878113bdb
Kevin Kang <kevinkangg@gmail.com>
2019-02-27 03:34:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,13 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :"pirates/new"
+    end
+
+    get '/new' do
+      erb :"pirates/form"
+    end
 
   end
 end
