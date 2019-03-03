6903bd404c8069e7000345c7712e700a9fa1080f
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-03-03 10:45:15 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,11 @@
 class Pirate
+  @@all = []
+  attr_accessor :name, :weight, :height
+  def initialize(name, weight, height)
+    @name, @weight, @height = name, weight, height
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
 end
