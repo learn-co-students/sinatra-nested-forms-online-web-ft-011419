bf38eb1c73d5f9a99638fae3fdeb052da27a19da
luiszaba <luis.zabala076@gmail.com>
2019-03-05 00:34:48 -0500

    i dont know whats wrong here

--- Gemfile
+++ Gemfile
@@ -2,14 +2,13 @@ source "https://rubygems.org"
 
 gem 'sinatra', require: 'sinatra/base'
 gem 'sinatra-reloader', require: false
-gem 'activerecord', require => 'active_record'
-gem 'sintara-activerecord', :require => 'sintara/activerecord'
 gem 'sqlite3'
 gem 'shotgun'
 gem 'pry'
 gem 'tux'
 gem 'require_all'
 gem 'rake'
+gem 'rails'
 
 group :test do
   gem 'rspec'
--- app.rb
+++ app.rb
@@ -6,7 +6,20 @@ module FormsLab
     # code other routes/actions here
 
     get '/' do
-      erb :new
+      erb :root
+    end
+
+    get '/new' do
+      erb :pirates/new
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      params[:pirate][:ships].each do |info|
+      Ship.new(info)
+    end
+    @ships = Ship.all
+    erb :'pirates/show'
   end
 end
 end
