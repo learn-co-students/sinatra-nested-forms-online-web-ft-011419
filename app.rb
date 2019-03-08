ccedc77a36c58f9245463600e18f651a7735cccc
Michael Schiumo <mschiumo18@gmail.com>
2019-03-08 13:51:58 -0500

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,24 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      erb :root
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ship].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
+
+      erb :new
+    end
+
+    get '/new' do
+
+    end
+
   end
 end
